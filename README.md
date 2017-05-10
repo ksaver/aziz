680ecc948e4c6471ae29f42cdea6278460e31590
ksaver <ks4v3r@gmail.com>
2017-05-10 03:27:03 -0500

    Initial commit. Aziz works just fine.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Aziz.py
+A simple [tkinter](https://docs.python.org/3/library/tk.html) [GUI](https://en.wikipedia.org/wiki/Graphical_user_interface) for [xbacklight](https://linux.die.net/man/1/xbacklight) command
+
+---
+
+The name Aziz makes reference to [The Fift Element](http://www.imdb.com/title/tt0119116/) character.
+
+[![Aziz light!!](http://img.youtube.com/vi/mvwd13F_1Gs/0.jpg)](http://www.youtube.com/watch?v=mvwd13F_1Gs)
+
+The program main interface uses a ["scale"](http://effbot.org/tkinterbook/scale.htm) tkinter widget to adjust backlight brightness.
+
+![Aziz 1.0](./aziz.png)
+
+---
+
+## More info:
+* [Tkinter:](https://en.wikipedia.org/wiki/Tkinter)
+* [xbacklight](https://www.x.org/archive/X11R7.5/doc/man/man1/xbacklight.1.html)
+* [The Fifht Element (1997)](http://www.imdb.com/title/tt0119116/)
---
+++ aziz.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+#  Aziz.py
+#  tkinter gui for xbacklight command.
+#  "Aziz Light!" 
+
+import os
+import subprocess
+from math import ceil
+from tkinter import *
+
+scriptname = "Aziz 1.0"
+        
+def get_backlight():
+    level = subprocess.check_output(['xbacklight', '-get'])
+    level = level.decode('utf-8')
+    level = ceil(float(level))
+    return(level)
+
+def set_backlight(level):
+    command = "xbacklight -set " + str(level)
+    os.system(command)
+
+def reset_backlight():
+    previous_value = current_bl
+    scale.set(previous_value)
+    set_backlight(previous_value)
+
+
+current_bl = get_backlight()
+
+window = Tk()
+window.title("{}".format(scriptname))
+
+top_frame = Frame(window)
+middle_frame = Frame(window)
+bottom_frame = Frame(window)
+
+label = Label(top_frame, bd=4, anchor=CENTER, #relief="groove",
+              text="Set backlight level:", font="Verdana")
+label.grid(row=0, column=0)
+
+scale = Scale(middle_frame, from_=0, to=100,
+              command=set_backlight, orient=HORIZONTAL, width=12,
+              font="Verdana", relief="sunken",
+              length=220, sliderlength=50, troughcolor="#c3c3c3")
+scale.set(current_bl)
+scale.grid(row=1, column=0, padx=4, pady=4, columnspan=2)
+
+reset_button = Button(bottom_frame, text="Reset", command=reset_backlight)
+reset_button.grid(row=1, column=1)
+
+quit_button = Button(bottom_frame, text="Quit", command=window.destroy)
+quit_button.grid(row=1, column=2)
+
+top_frame.pack(padx=4, pady=4, ipadx=4, ipady=4)
+middle_frame.pack(padx=4, pady=4, ipadx=4, ipady=4)
+bottom_frame.pack(padx=4, pady=4, ipadx=4, ipady=4)
+
+window.mainloop()
